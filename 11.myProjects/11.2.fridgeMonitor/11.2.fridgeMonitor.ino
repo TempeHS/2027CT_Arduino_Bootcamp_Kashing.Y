9abbb9a925e388ed9a1c5e875b5e983cf391b5a5
leaf700 <kashing.yang@education.nsw.gov.au>
2026-09-08 00:09:52 +0000

    Finished fridge monitor, lights and buzzers activate

--- 11.myProjects/11.2.fridgeMonitor/11.2.fridgeMonitor.ino
+++ 11.myProjects/11.2.fridgeMonitor/11.2.fridgeMonitor.ino
@@ -2,16 +2,71 @@
 const int LIGHT_PIN = A3;
 const int LED_PIN = 6;
 const int BUZZER_PIN = 5;
+const int DOOR_OPEN_THRESHOLD = 450;
+
+const unsigned long DOOR_TIMEOUT   = 10000; // The door must be open for at least 10 seconds before the buzzer starts
+const unsigned long FLASH_INTERVAL = 250; // The lights will flash every 2.5 seconds while hte door is open
+const unsigned long PLOT_INTERVAL  = 50; //How frequently the code checks if the door is open or closed in milliseconds
+
+unsigned long doorOpenStartTime = 0; //Unsigned long means the long value cannot be negative, so the positive long value is doubled
+bool doorWasOpenLastCheck       = false; //Sets door was open last check to false at the start of the program, but not const so it can be changed when something happens
+bool alarmActive                = false; //Turns alarm off at teh start of the program
+
+unsigned long lastFlashTime = 0;
+unsigned long lastPlotTime  = 0;
+bool ledState               = LOW;
 
 void setup() {
   Serial.begin(115200);
+
+  pinMode(BUZZER_PIN, OUTPUT);
+  pinMode(LED_PIN, OUTPUT);
+
+  digitalWrite(BUZZER_PIN, LOW);
+  digitalWrite(LED_PIN, LOW);
 }
 
 void loop() {
-  Serial.println(analogRead(A3));
-  delay(50);
-}
+  unsigned long currentMillis = millis();
+  int lightLevel = analogRead(LIGHT_PIN);
+  bool isDoorOpen = (lightLevel > DOOR_OPEN_THRESHOLD); //For the code to decide if the door is open, the light sensor has to detect a light level of above the door open threshold
 
+  if (isDoorOpen) {
+    if (!doorWasOpenLastCheck) {
+      doorOpenStartTime = currentMillis;
+      doorWasOpenLastCheck = true;
+    }
+    if (currentMillis - doorOpenStartTime >= DOOR_TIMEOUT) {
+      alarmActive = true;
+    }
+  } else {
+    doorWasOpenLastCheck = false;
+    alarmActive = false;
+    digitalWrite(BUZZER_PIN, LOW);
+    digitalWrite(LED_PIN, LOW);
+  }
+  if (alarmActive) {
+    digitalWrite(BUZZER_PIN, HIGH);
+
+    if (currentMillis - lastFlashTime >= FLASH_INTERVAL) {
+      lastFlashTime = currentMillis;
+      ledState = !ledState;
+      digitalWrite(LED_PIN, ledState);
+    }
+  }
+
+  if (currentMillis - lastPlotTime >= PLOT_INTERVAL) {
+    lastPlotTime = currentMillis;
+    Serial.print("LightLevel:");
+    Serial.print(lightLevel);
+    Serial.print(",");
+    Serial.print("Threshold:");
+    Serial.print(DOOR_OPEN_THRESHOLD);
+    Serial.print(",");
+    Serial.print("DoorState:");
+    Serial.println(isDoorOpen ? 500 : 0);
+  }
+}
 
 
 
