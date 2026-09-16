39f27ae7a417d742c98bd2c1da18148c62317a32
leaf700 <kashing.yang@education.nsw.gov.au>
2026-09-16 04:10:07 +0000

    WIP fidge monitor

--- 11.myProjects/11.2.fridgeMonitor/11.2.fridgeMonitor.ino
+++ 11.myProjects/11.2.fridgeMonitor/11.2.fridgeMonitor.ino
@@ -1,80 +1,3 @@
-
-const int LIGHT_PIN = A3;
-const int LED_PIN = 6;
-const int BUZZER_PIN = 5;
-
-const int DOOR_OPEN_THRESHOLD = 2; //If light sensor detects this level of light or higher, the door state will be considered open
-const unsigned long DOOR_TIMEOUT = 10000; //If the door is open for 10 seconds
-const unsigned long FLASH_INTERVAL = 250; //Intervals between the LED flashing in milliseconds
-const unsigned long PLOT_INTERVAL = 50;  //How often the data is plotted/checked
-
-unsigned long doorOpenStartTime = 0;
-bool doorWasOpenLastCheck = false; //Setting up the light and alarm to not start immediately
-bool alarmActive = false;
-
-unsigned long lastFlashTime = 0;
-unsigned long lastPlotTime = 0;
-bool ledState = LOW; //Making sure the light does not start before the door is considered open
-
-void setup() {
-  Serial.begin(115200);
-  pinMode(BUZZER_PIN, OUTPUT); //Supply electricity to the buzzer and LED pins
-  pinMode(LED_PIN, OUTPUT);
-  digitalWrite(BUZZER_PIN, LOW); //Turn them off at the start of the program
-  digitalWrite(LED_PIN, LOW);
-}
-
-void loop() {
-  unsigned long currentMillis = millis();
-  int lightLevel = analogRead(LIGHT_PIN); //Convert voltage into a digital number, int lightLevel stores that data
-  bool isDoorOpen = (lightLevel > DOOR_OPEN_THRESHOLD); //The door being open is decided by if the door open threshold is lower than the light level
-
-  if (isDoorOpen) {
-    if (!doorWasOpenLastCheck) {
-      doorOpenStartTime = currentMillis;
-      doorWasOpenLastCheck = true;
-    }
-
-    if (currentMillis - doorOpenStartTime >= DOOR_TIMEOUT) {
-      alarmActive = true;
-    }
-  } else {
-    doorWasOpenLastCheck = false;
-    alarmActive = false; //If the door is open but hasn't been open for ten seconds, the alarm will not activate
-    digitalWrite(BUZZER_PIN, LOW); //Making the buzzer and led turn off
-    digitalWrite(LED_PIN, LOW);
-  }
-
-  if (alarmActive) {
-    digitalWrite(BUZZER_PIN, HIGH); //Turning the buzzer on if the door is open for 10 seconds or more
-
-    if (currentMillis - lastFlashTime >= FLASH_INTERVAL) {
-      lastFlashTime = currentMillis;
-      ledState = !ledState;
-      digitalWrite(LED_PIN, ledState);
-    }
-  }
-
-  if (currentMillis - lastPlotTime >= PLOT_INTERVAL) {
-    lastPlotTime = currentMillis;
-    Serial.print("LightLevel:");
-    Serial.print(lightLevel);
-    Serial.print(",");
-    Serial.print("Threshold:");
-    Serial.print(DOOR_OPEN_THRESHOLD);
-    Serial.print(",");
-    Serial.print("DoorState:");
-    Serial.println(isDoorOpen ? 500 : 0);
-  }
-}
-
-
-
-
-
-
-
-
 /*
   Author:
   Learning Intention: Students will build a monitoring system that watches
@@ -84,9 +7,9 @@ void loop() {
   1. Calibrate the light sensor FIRST (see below), then set your threshold
   2. The light sensor detects the door opening (light floods into a dark fridge)
   3. If the door stays open longer than 10 seconds, sound the buzzer and flash
-     the LED until the door closes
+the LED until the door closes
   4. Keep the loop non-blocking: use the millis() pattern for the alarm flash
-     AND the Serial Plotter output (no delay() anywhere)
+AND the Serial Plotter output (no delay() anywhere)
   5. Plot the light level and door state so you can watch your threshold work
 
   Calibration (why: every bench is different):
@@ -105,4 +28,54 @@ void loop() {
 
   Suggested Grove ports: Light A3, Buzzer D5, LED D6
 */
-//Value of door open is 5 while value of door closed is 0, door open threshold will be 2
+const int LIGHT_SENSOR_PIN = A3;
+const int LED_PIN = 6;
+const int LIGHT_THRESHOLD = 200;
+const unsigned long FRIDGE_OPEN_TIME_LIMIT = 10000;
+const unsigned long FLASH_INTERVAL = 200;
+
+unsigned long lightStartedAt = 0;
+unsigned long lastFlashTime = 0;
+bool lightIsOn = false;
+bool ledState = LOW;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(LED_PIN, OUTPUT);
+}
+
+void loop() {
+  int lightLevel = analogRead(LIGHT_SENSOR_PIN);
+  bool doorIsOpen = lightLevel > LIGHT_THRESHOLD;
+
+  Serial.print(lightLevel);
+  Serial.print(",");
+  Serial.println(doorIsOpen ? 1 : 0);
+
+  if (doorIsOpen) {
+    // 1. Record the exact time the door was first opened
+    if (!lightIsOn) {
+      lightStartedAt = millis();
+      lightIsOn = true;
+    }
+
+    // 2. Separately check if the door has been open past the limit
+    if (millis() - lightStartedAt >= FRIDGE_OPEN_TIME_LIMIT) {
+      if (millis() - lastFlashTime >= FLASH_INTERVAL) {
+        lastFlashTime = millis();
+        ledState = !ledState;
+        digitalWrite(LED_PIN, ledState);
+      }
+    } else {
+      // Keep LED off while waiting for the timer to expire
+      digitalWrite(LED_PIN, LOW);
+    }
+  } else {
+    // Reset all states completely when the door is closed
+    lightIsOn = false;
+    ledState = LOW;
+    digitalWrite(LED_PIN, LOW);
+  }
+
+  delay(50);
+}
